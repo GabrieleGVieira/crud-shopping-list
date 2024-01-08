f8af6ef260553a8a97a17d625ac14f657d4089fd
Gabriele Gonçalves Vieira <gabrielevieira011@gmail.com>
2024-01-08 17:44:05 -0300

    feat: create form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,9 +3,23 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import styles from "./formStyles"
 
-export default function Form() {
+export default function Form({navigation}) {
 
-  
+  const [description, setDescription] = useState('')
+  const [amount, setAmount] = useState(0)
+
+  function handleDescriptionChange(description) {
+    setDescription(description);;
+  }
+
+  function handleAmountChange(amount) {
+    setAmount(amount)
+  }
+
+  function handleButtonPress() {
+    console.log({ id: new Date().getTime(), description, amount });
+    navigation.navigate("List")
+  }
 
     return (
       <View style={styles.container}>
@@ -15,14 +29,16 @@ export default function Form() {
             style={styles.input}
             placeholder="O que está faltando em casa?"
             clearButtonMode="always"
+            onChangeText={handleDescriptionChange}
           />
           <TextInput
             style={styles.input}
             placeholder="Digite a quantidade"
             keyboardType={"numeric"}
             clearButtonMode="always"
+            onChangeText={handleAmountChange}
           />
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity style={styles.button} onPress={handleButtonPress}>
             <Text style={styles.buttonText}>Salvar</Text>
           </TouchableOpacity>
         </View>
