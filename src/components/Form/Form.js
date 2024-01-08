e63a30202b1e072daf84eacdd1ed6349fcbacc21
Gabriele Gonçalves Vieira <gabrielevieira011@gmail.com>
2024-01-08 19:45:32 -0300

    feat: creating delete function

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,67 +1,70 @@
 import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, View, TextInput, TouchableOpacity } from "react-native";
-import styles from "./formStyles"
+import styles from "./formStyles";
 
-import db from "../../../database/db"
+import db from "../../../database/db";
 
 export default function Form({ route, navigation }) {
-  
   // Create
 
-  const [description, setDescription] = useState('')
-  const [amount, setAmount] = useState("")
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
 
   function handleDescriptionChange(description) {
-    setDescription(description);;
+    setDescription(description);
   }
 
   function handleAmountChange(amount) {
-    setAmount(amount)
+    setAmount(amount);
   }
 
   async function handleButtonPress() {
-    const listItem = { id: new Date().getTime(), description, amount: parseInt(amount)};
-    
-    db.saveItem(listItem).then(response => navigation.navigate("List", listItem))
+    const listItem = {
+      id: new Date().getTime(),
+      description,
+      amount: parseInt(amount),
+    };
+
+    db.saveItem(listItem, id).then((response) =>
+      navigation.navigate("List", listItem)
+    );
   }
 
-   //Update
+  //Update
 
-  const id = route.params ? route.params.id : undefined
+  const id = route.params ? route.params.id : undefined;
 
   useEffect(() => {
     if (!route.params) return;
-    setDescription(route.params.description)
+    setDescription(route.params.description);
     setAmount(route.params.amount.toString());
-  }, [route])
+  }, [route]);
 
-
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Item Para Compra</Text>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="O que está faltando em casa?"
-            clearButtonMode="always"
-            onChangeText={handleDescriptionChange}
-            value={description}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Digite a quantidade"
-            keyboardType={"numeric"}
-            clearButtonMode="always"
-            onChangeText={handleAmountChange}
-            value={amount}
-          />
-          <TouchableOpacity style={styles.button} onPress={handleButtonPress}>
-            <Text style={styles.buttonText}>Salvar</Text>
-          </TouchableOpacity>
-        </View>
-        <StatusBar style="light" />
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Item Para Compra</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="O que está faltando em casa?"
+          clearButtonMode="always"
+          onChangeText={handleDescriptionChange}
+          value={description}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Digite a quantidade"
+          keyboardType={"numeric"}
+          clearButtonMode="always"
+          onChangeText={handleAmountChange}
+          value={amount}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleButtonPress}>
+          <Text style={styles.buttonText}>Salvar</Text>
+        </TouchableOpacity>
       </View>
-    );
+      <StatusBar style="light" />
+    </View>
+  );
 }
-
--- src/components/List/Item/Item.js
+++ src/components/List/Item/Item.js
@@ -1,19 +1,45 @@
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, Alert } from "react-native";
 import styles from "./itemStyles";
 import db from "../../../../database/db";
 
 export default function ItemList(props) {
-
   async function handleEditPress() {
     const item = await db.getItem(props.id);
-    props.navigation.navigate("Form", item)
+    props.navigation.navigate("Form", item);
   }
+
+  async function handleDeletePress() {
+    Alert.alert(
+      "Atenção",
+      "Você tem certeza que deseja excluir este item?",
+      [
+        {
+          text: "Não",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        {
+          text: "Sim",
+          onPress: () => {
+            db.deleteItem(props.id).then((response) =>
+              props.navigation.navigate("List", { id: props.id })
+            );
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.textItem}>{props.item}</Text>
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={styles.deleteButton}>
+        <TouchableOpacity
+          style={styles.deleteButton}
+          onPress={handleDeletePress}
+        >
           <Text style={styles.buttonText}>X</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.editButton} onPress={handleEditPress}>
@@ -22,4 +48,4 @@ export default function ItemList(props) {
       </View>
     </View>
   );
-};
+}
